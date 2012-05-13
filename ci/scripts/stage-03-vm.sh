70c8e6eccf6b2422ab95612a47b5839ed8870877
Christian Stocker <me@chregu.tv>
2012-05-13 22:11:02 +0200

    sudo for now

--- ci/scripts/stage-03-vm.sh
+++ ci/scripts/stage-03-vm.sh
@@ -1,5 +1,5 @@
 vagrant up
 vagrant provision
 vagrant ssh -c "cp /vagrant/ci/build/debs/jackalope-jackrabbit_0.9.deb . && sudo dpkg -i jackalope-jackrabbit_0.9.deb"
-vagrant ssh -c "cd /var/www/jackalope-jackrabbit &&  ./bin/jackrabbit.sh &&  phpunit -c tests/phpunit.xml.dist"
+vagrant ssh -c "cd /var/www/jackalope-jackrabbit &&  sudo ./bin/jackrabbit.sh &&  sudo phpunit -c tests/phpunit.xml.dist"
 vagrant halt
