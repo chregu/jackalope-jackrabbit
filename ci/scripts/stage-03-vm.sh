320cce31d929b156014566ae9bbae2a392b197a9
Christian Stocker <me@chregu.tv>
2012-05-14 07:03:55 +0200

    correct rollback command

--- ci/scripts/stage-03-vm.sh
+++ ci/scripts/stage-03-vm.sh
@@ -2,6 +2,6 @@ vagrant up
 vagrant provision
 vagrant sandbox on
 vagrant ssh -c "bash /vagrant/ci/scripts/stage-03a-within-vm.sh"
-vagrant rollback
+vagrant sandbox rollback
 vagrant sandbox off
 vagrant halt
