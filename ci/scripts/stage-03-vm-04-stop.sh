ad3238eab43d9e25e8b6a26f69b21b06f5aa2696
Christian Stocker <me@chregu.tv>
2012-05-14 09:51:44 +0200

    tunro sandbox off off for now

--- ci/scripts/stage-03-vm-04-stop.sh
+++ ci/scripts/stage-03-vm-04-stop.sh
@@ -3,5 +3,7 @@
 # plesae do not edit test without talking to the test admin
 # this could be replaced by a system wide script on bamboo later
 vagrant sandbox rollback 2>> /tmp/vagrant.log
-vagrant sandbox off
+
+#no sandbox off for now, takes too long, should be "destroy and up" in a nightly/weekly cron or something
+#vagrant sandbox off
 vagrant halt
